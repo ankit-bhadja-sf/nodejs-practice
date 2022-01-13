df1b069106a7cb1e949b0a15afb6e3f4d71631fd
ankit bhadja <ankit.bhadaja@seaflux.tech>
2022-01-13 15:33:52 +0530

    [refactor] Refactored code

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res,) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(null, title, imageUrl, price, description)
+    const product = new Product( title, imageUrl, price, description)
     product.save();
     res.redirect('/');
 }
@@ -47,14 +47,15 @@ exports.postEditProduct = (req, res) => {
     const updatedImageUrl = req.body.imageUrl;
     const updatedPrice = req.body.price;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
+    const updateProducts = new Product(
         prodId, 
         updatedTitle, 
         updatedImageUrl, 
         updatedPrice, 
         updatedDesc
         );
-        updatedProduct.save();
+        updateProducts.save();
+        //res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res) => {
