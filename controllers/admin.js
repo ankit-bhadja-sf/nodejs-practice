2734e963dd9719e6410a5a32dda98612c9cd642e
ankit bhadja <ankit.bhadaja@seaflux.tech>
2022-01-13 11:48:00 +0530

    [fix] edit-menu

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -14,7 +15,7 @@ exports.postAddProduct = (req, res,) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, imageUrl, price, description)
+    const product = new Product(null, title, imageUrl, price, description)
     product.save();
     res.redirect('/');
 }
@@ -36,6 +37,24 @@ exports.getEditProduct = (req, res, next) => {
             product: product
     });
   });
+};
+
+///////////////////////////////Admin edit Product
+
+exports.postEditProduct = (req, res) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedPrice = req.body.price;
+    const updatedDesc = req.body.description;
+    const updatedProduct = new Product(
+        prodId, 
+        updatedTitle, 
+        updatedImageUrl, 
+        updatedPrice, 
+        updatedDesc
+        );
+        updatedProduct.save();
 };
 
 exports.getProducts = (req, res) => {
