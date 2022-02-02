caab0aed79aa3bd1ff866a158d886cb0101175a7
ankit bhadja <ankit.bhadaja@seaflux.tech>
2022-02-02 18:03:09 +0530

    [feature] complete-authentication-section

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,6 @@ exports.getAddProduct = (req, res) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    isAuthenticated: req.session.isLoggedIn
   });
 };
 
@@ -54,7 +53,6 @@ exports.getEditProduct = (req, res) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
-        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(er));
@@ -96,7 +94,6 @@ exports.getProducts = (req, res) => {
         prods: products,
         pageTitle: 'Admin Products',
         path: '/admin/products',
-        isAuthenticated: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
--- routes/shop.js
+++ routes/shop.js
@@ -6,22 +6,24 @@ const router = express.Router();
 
 const shopController = require('../controllers/shop.js');
 
+const isAuth = require('../middleware/is-auth')
 
-router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get('/', isAuth, shopController.getIndex);
 
-router.get('/products/:productId', shopController.getProduct);
+router.get('/products', isAuth, shopController.getProducts);
 
-router.get('/cart', shopController.getCart);
+router.get('/products/:productId', isAuth, shopController.getProduct);
 
-router.post('/cart', shopController.postCart);
+router.get('/cart', isAuth, shopController.getCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart', isAuth, shopController.postCart);
 
-router.post('/create-order', shopController.postOrder);
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+router.post('/create-order', isAuth, shopController.postOrder);
+
+router.get('/orders', isAuth, shopController.getOrders);
 
 
 module.exports = router
